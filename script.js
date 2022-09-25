5a034cd0b6f229c259c845f0548964f5e6c70cec
n <n@gmail.com>
2022-09-25 20:35:36 +0300

    final

---
+++ script.js
@@ -0,0 +1,9 @@
+var name = prompt('Введите свое имя')
+var year = +prompt('Введите год рождения')
+var now = +prompt('Введите нынешний год')
+/*var res = prompt(now - year)*/
+/*console.log(name + ' , Ваш возраст' + now - year)*/
+alert(name + ', Ваш возраст ' + (now - year))
+
+
+
